41924dad7a4ce44a5cdbf905d977b3866f5c06d1
Goyder <joshua@goyder.net.au>
2018-02-10 20:37:46 +0800

    Working docker-compose set-upd

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,5 @@
 FROM mysql
 
-ADD files/init_db files/run_db files/schema.sql /tmp/
 
-RUN /tmp/init_db
-
-ENTRYPOINT "/tmp/run_db"
 
 
--- files/schema.sql
+++
@@ -1,9 +0,0 @@
-create database test;
-use test;
-
-CREATE TABLE testtab
-(
-id INTEGER AUTO_INCREMENT,
-name TEXT,
-PRIMARY KEY (id)
-) COMMENT='it's a test table, son';
