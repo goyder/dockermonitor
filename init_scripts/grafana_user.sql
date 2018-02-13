91a77fa9764bd7805e81c1208eed4614d720a555
Goyder <joshua@goyder.net.au>
2018-02-13 21:24:05 +0800

    Grafana user actually working. Add readme.md.

--- init_scripts/grafana_user.sql
+++ init_scripts/grafana_user.sql
@@ -1,3 +1,6 @@
 -- Create a limited user for accessing the MySQL database from Grafana.
-CREATE USER IF NOT EXISTS 'grafana' IDENTIFIED BY 'grafana';
-GRANT SELECT ON main_db.data TO 'grafana';
+CREATE USER IF NOT EXISTS 'grafana'@'%' IDENTIFIED BY 'grafana';
+DROP USER IF EXISTS ''@'%';
+USE main_db;
+GRANT SELECT ON main_db.* TO 'grafana'@'%';
+FLUSH TABLES mysql.user;
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Docker Monitor
+* Goyder 2018 * 
+
+A basic project, build primarily in Docker containers, to link together a host of technologies.
+
+The goal is to have environmental sensors talking to an MQTT server, with the data being captured in a database and interrogated in a Grafana server.
+
+Let's see how that goes.
