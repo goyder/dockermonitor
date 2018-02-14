c74a7c8ada8142702443e25fa9b43e9ef4aad269
Goyder <joshua@goyder.net.au>
2018-02-14 20:36:07 +0800

    Revise grafana_user.sql

--- init_scripts/grafana_user.sql
+++ init_scripts/grafana_user.sql
@@ -1,6 +1,7 @@
 -- Create a limited user for accessing the MySQL database from Grafana.
+-- Commented out items were assumed to be necessary, but later found to be redundant.
 CREATE USER IF NOT EXISTS 'grafana'@'%' IDENTIFIED BY 'grafana';
-DROP USER IF EXISTS ''@'%';
-USE main_db;
+-- DROP USER IF EXISTS ''@'%';
+-- USE main_db;
 GRANT SELECT ON main_db.* TO 'grafana'@'%';
-FLUSH TABLES mysql.user;
+-- FLUSH TABLES mysql.user;
